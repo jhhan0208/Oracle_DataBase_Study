dfdf3c96e807411ad29ae3e97e95dcdede60021f
jhhan0208 <67776091+jhhan0208@users.noreply.github.com>
2024-04-03 13:48:42 +0900

    Update 1_1_Writing Basic SQL.sql

--- 1_1_Writing Basic SQL.sql
+++ 1_1_Writing Basic SQL.sql
@@ -5,10 +5,10 @@ select * from DEPARTMENTS
 
 -- 2. EMPLOYEES 테이블의 내용을 검색시 사원번호, 사원이름, 업무코드, 입사일이 오도록 질의를 작성한다. HIRE_DATE 열에 STARTDATE라는 별칭을 지정한다.
 
-select EMPLOYEE_ID, FIRST_NAME, LAST_NAME, JOB_ID, HIRE_DATE AS "STARTDATE"
+select EMPLOYEE_ID, LAST_NAME, JOB_ID, HIRE_DATE AS "STARTDATE"
 from EMPLOYEES
 
 -- 3. 이름과 JOB_ID를 연결한 다음 쉼표 및 공백으로 구분하여 표시하고, 열이름을 Employee and Title로 지정한다.
 
-select FIRST_NAME || ', ' || JOB_ID AS "Employee and Title"
+select LAST_NAME || ', ' || JOB_ID AS "Employee and Title"
 from EMPLOYEES
